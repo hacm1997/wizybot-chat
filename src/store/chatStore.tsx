0be5eabf9693fa5e659ec2dba1c25c8a7386cb0e
hacm1997 <hacm1997@gmail.com>
2025-05-08 09:53:29 -0500

    feat: refactor storage, now recived user name param

--- src/store/chatStore.tsx
+++ src/store/chatStore.tsx
@@ -1,7 +1,7 @@
-// store/chatStore.ts
 import { create } from "zustand";
 import type { Message } from "../types/message";
 import { getCurrentTime } from "../utils/getCurrentTime";
+import { v4 as uuidv4 } from 'uuid';
 
 type ChatState = {
     messages: Message[];
@@ -10,22 +10,27 @@ type ChatState = {
     setMessages: (msgs: Message[]) => void;
     setInput: (value: string) => void;
     setIsTyping: (val: boolean) => void;
+    initializeChat: (name: string) => void;
 };
 
-// Zustand Storage for chat persistence when user minimize the chat
+// Zustand Storage for chat persistence when user minimize the chat (without reload page)
 export const useChatStore = create<ChatState>((set) => ({
-    messages: [
-        {
-            id: 1,
-            text: "Hello there! Do you need any help?",
-            position: "left",
-            type: "text",
-            timestamp: getCurrentTime(),
-        },
-    ],
+    messages: [],
     input: "",
     isTyping: false,
     setMessages: (msgs) => set({ messages: msgs }),
-    setInput: (value) => set({ input: value }),
+    setInput: (val) => set({ input: val }),
     setIsTyping: (val) => set({ isTyping: val }),
+    initializeChat: (name: string) =>
+        set({
+            messages: [
+                {
+                    id: uuidv4(),
+                    text: `Hello ${name.toUpperCase()}! Do you need any help?`,
+                    position: "left",
+                    type: "text",
+                    timestamp: getCurrentTime(),
+                },
+            ],
+        }),
 }));
