a2d79ec973a1c5a177f0a0c7c1a6bedbb3010a99
hacm1997 <hacm1997@gmail.com>
2025-05-08 09:52:38 -0500

    feat: Memoizes ProductCarousel to avoid unnecessary rendering

--- src/components/molecules/product-card/ProductCard.tsx
+++ src/components/molecules/product-card/ProductCard.tsx
@@ -1,5 +1,6 @@
 import type { ProductCardProps } from "./product-card.model";
 import styles from '../../../utils/styles.module.css';
+import { generateRandomPrice } from "../../../utils/generateRandomPrice";
 
 {/* Dynamic Product Card */ }
 export default function ProductCard({ product }: ProductCardProps) {
@@ -7,13 +8,13 @@ export default function ProductCard({ product }: ProductCardProps) {
     return (
         <div className={styles.customBoxShadow + " border border-transparent rounded-lg p-4 shadow w-full bg-amber-50"}>
             <div className="flex gap-4 w-[250px]">
-                <img src={product.image} alt={product.name} className="w-[80px] h-[100px] object-cover rounded" />
+                <img src={product.image} alt={product.name} className="w-[80px] h-[100px] object-cover rounded" loading="lazy" />
                 <div className="text-left">
                     <h4 className="mt-2 font-semibold">
                         {/* Minumun 14 characteres to display for the product name */}
                         {product.name.length > 14 ? product.name.slice(0, 14) + '...' : product.name}
                     </h4>
-                    <p className="text-sm text-black font-semibold">$25.00</p>
+                    <p className="text-sm text-black font-semibold">${generateRandomPrice()}</p>
                     <p className="text-sm text-gray-600">Type: {product.type}</p>
                     <a href={product.url} target="_blank" className="text-black underline text-sm mt-1 block">
                         View Page
--- src/components/organisms/product-carousel/ProductCarousel.tsx
+++ src/components/organisms/product-carousel/ProductCarousel.tsx
@@ -1,12 +1,16 @@
+import React from "react";
 import ProductCard from "../../molecules/product-card/ProductCard";
 import type { ProductCarouselProps } from "./product-carousel.model";
 
-export default function ProductCarousel({ products }: ProductCarouselProps) {
+// Memoizes ProductCarousel to avoid unnecessary rendering
+const ProductCarousel = React.memo(function ProductCarousel({ products }: ProductCarouselProps) {
     return (
-        // Dynamic Product Card Slider
-        <div className="flex space-x-4 overflow-x-scroll p-2">
+        <div className="flex space-x-4 overflow-x-scroll p-2 scrollChatContainer">
             {products.map((product) => (
                 <ProductCard key={product.id} product={product} />
             ))}
-        </div>)
-}
+        </div>
+    );
+});
+
+export default ProductCarousel;
